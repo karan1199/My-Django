b693171650b79727b8b46c5b01da679eb7af2e1e
Karan Patel <karan11119999@gmail.com>
2020-10-05 22:17:29 +0530

    initial6

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -28,8 +28,8 @@ navLink.forEach(n => n.addEventListener('click', linkAction));
 /*===== SCROLL REVEAL ANIMATION =====*/
 const sr = ScrollReveal({
     origin: 'top',
-    distance: '80px',
-    duration: 1000,
+    distance: '20px',
+    duration: 700,
     reset: true
 });
 
